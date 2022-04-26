e5fae4aa1408d96e6e4357bd4f0393f21f668947
Lois <lois.gigaud85@gmail.com>
2022-04-26 16:24:49 +0200

    Ajout obfuscation sur authentification

--- lean-co-test/src/app/app.module.ts
+++ lean-co-test/src/app/app.module.ts
@@ -6,12 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecureCodeService } from './secure/secure-code.service';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SecureCodeService],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ lean-co-test/src/app/secure/secure-code.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SecureCodeService } from './secure-code.service';
+
+describe('SecureCodeService', () => {
+  let service: SecureCodeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SecureCodeService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
